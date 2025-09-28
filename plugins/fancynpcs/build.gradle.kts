57123c6deffbef926e43074dea7571f4bf342a8f
Oliver <oschi1303@gmail.com>
2025-09-28 13:17:36 +0200

    fancynpcs: Use 2.0.0 for cloud

--- plugins/fancynpcs/build.gradle.kts
+++ plugins/fancynpcs/build.gradle.kts
@@ -81,10 +81,10 @@ dependencies {
     implementation("de.oliver.FancyAnalytics:java-sdk:0.0.4")
     implementation("de.oliver.FancyAnalytics:mc-api:0.1.11")
     implementation("de.oliver.FancyAnalytics:logger:0.0.8")
-    implementation("org.incendo:cloud-core:2.1.0-SNAPSHOT")
+    implementation("org.incendo:cloud-core:2.0.0")
     implementation("org.incendo:cloud-paper:2.0.0-beta.11")
-    implementation("org.incendo:cloud-annotations:2.1.0-SNAPSHOT")
-    annotationProcessor("org.incendo:cloud-annotations:2.1.0-SNAPSHOT")
+    implementation("org.incendo:cloud-annotations:2.0.0")
+    annotationProcessor("org.incendo:cloud-annotations:2.0.0")
     implementation("org.mineskin:java-client-jsoup:3.0.3-SNAPSHOT")
 
     compileOnly("me.clip:placeholderapi:2.11.6")

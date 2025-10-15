1ea1033b2aea6d1508671929059d33fbb1f8d3a9
Oliver <oschi1303@gmail.com>
2025-10-15 18:15:12 +0200

    deployment: Add jwt auth

--- tools/deployment/src/main/java/de/oliver/deployment/hangar/HangarService.java
+++ tools/deployment/src/main/java/de/oliver/deployment/hangar/HangarService.java
@@ -1,29 +1,36 @@
 package de.oliver.deployment.hangar;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import de.oliver.deployment.Configuration;
 import de.oliver.deployment.git.GitService;
+import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HttpMessage;
 import org.apache.hc.core5.http.ParseException;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class HangarService {
 
     private static final Gson GSON = new Gson();
+    private static final String HANGAR_API_URL = "https://hangar.papermc.io/api/v1";
 
     private final String apiKey;
+    private ActiveJWT activeJWT;
 
     public HangarService(String apiKey) {
         this.apiKey = apiKey;
@@ -52,8 +59,8 @@ public class HangarService {
         // Create HTTP client
         try (CloseableHttpClient client = HttpClients.createDefault()) {
 
-            HttpPost post = new HttpPost("https://hangar.papermc.io/api/v1/projects/" + config.projectName() + "/upload");
-            post.addHeader("Authorization", "Bearer " + apiKey);
+            HttpPost post = new HttpPost(HANGAR_API_URL + "/projects/" + config.projectName() + "/upload");
+            this.addAuthorizationHeader(client, post);
 
             // Build multipart entity
             MultipartEntityBuilder builder = MultipartEntityBuilder.create();
@@ -82,6 +89,52 @@ public class HangarService {
             } catch (ParseException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private synchronized void addAuthorizationHeader(final HttpClient client, final HttpMessage message) throws IOException {
+        if (this.activeJWT != null && !this.activeJWT.hasExpired()) {
+            // Add the active JWT
+            message.addHeader("Authorization", this.activeJWT.jwt());
+            return;
+        }
+
+        // Request a new JWT
+        final ActiveJWT jwt = client.execute(new HttpPost("%s/authenticate?apiKey=%s".formatted(HANGAR_API_URL, this.apiKey)), response -> {
+            if (response.getCode() == 400) {
+                System.out.println("Bad JWT request; is the API key correct?");
+                return null;
+            } else if (response.getCode() != 200) {
+                System.out.println("Error requesting JWT:" + response.getReasonPhrase());
+                return null;
+            }
+
+            final String json = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+            final JsonObject object = GSON.fromJson(json, JsonObject.class);
+            final String token = object.getAsJsonPrimitive("token").getAsString();
+            final long expiresIn = object.getAsJsonPrimitive("expiresIn").getAsLong();
+            return new ActiveJWT(token, System.currentTimeMillis() + expiresIn);
+        });
+
+        if (jwt == null) {
+            throw new RuntimeException("Error getting JWT");
+        }
+
+        this.activeJWT = jwt;
+        message.addHeader("Authorization", jwt.jwt());
+    }
+
+    /**
+     * Represents an active JSON Web Token used for authentication with Hangar.
+     *
+     * @param jwt       Active JWT
+     * @param expiresAt time in milliseconds when the JWT expires
+     */
+    private record ActiveJWT(String jwt, long expiresAt) {
+
+        public boolean hasExpired() {
+            // Make sure we request a new one before it expires
+            return System.currentTimeMillis() < this.expiresAt + TimeUnit.SECONDS.toMillis(3);
         }
     }
 

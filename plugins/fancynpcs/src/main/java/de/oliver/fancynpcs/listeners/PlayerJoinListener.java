bce7ae9651c4dfb0a955e6d4397d60ef7933edd7
TheosRee <theosree@users.noreply.github.com>
2025-11-21 16:01:55 +0100

    fancynpcs: remove the Player UUID from the Npc maps instead setting it to default value also don't set the default value on Join remove code comments in favor of JDoc for method

--- plugins/fancynpcs/src/main/java/de/oliver/fancynpcs/listeners/PlayerJoinListener.java
+++ plugins/fancynpcs/src/main/java/de/oliver/fancynpcs/listeners/PlayerJoinListener.java
@@ -2,7 +2,6 @@ package de.oliver.fancynpcs.listeners;
 
 import com.destroystokyo.paper.profile.ProfileProperty;
 import de.oliver.fancynpcs.FancyNpcs;
-import de.oliver.fancynpcs.api.Npc;
 import de.oliver.fancynpcs.api.skins.SkinData;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,12 +11,6 @@ public class PlayerJoinListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        for (Npc npc : FancyNpcs.getInstance().getNpcManagerImpl().getAllNpcs()) {
-            npc.getIsVisibleForPlayer().put(event.getPlayer().getUniqueId(), false);
-            npc.getIsLookingAtPlayer().put(event.getPlayer().getUniqueId(), false);
-            npc.getIsTeamCreated().put(event.getPlayer().getUniqueId(), false);
-        }
-
         // don't spawn the npc for player if he just joined
         FancyNpcs.getInstance().getVisibilityTracker().addJoinDelayPlayer(event.getPlayer().getUniqueId());
         FancyNpcs.getInstance().getScheduler().runTaskLater(null, 20L * 2, () -> FancyNpcs.getInstance().getVisibilityTracker().removeJoinDelayPlayer(event.getPlayer().getUniqueId()));

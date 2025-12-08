38f92681129b366f1fed336c3287a52edaf4d591
Oliver <oschi1303@gmail.com>
2025-12-08 17:53:06 +0100

    Add support for 1.21.11-rc3

--- libraries/packets/implementations/1_21_11/build.gradle.kts
+++ libraries/packets/implementations/1_21_11/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
 
 dependencies {
-    paperweight.paperDevBundle("1.21.11-rc2-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("1.21.11-rc3-R0.1-SNAPSHOT")
     compileOnly(project(":libraries:packets:packets-api"))
 
     testImplementation(project(":libraries:packets"))

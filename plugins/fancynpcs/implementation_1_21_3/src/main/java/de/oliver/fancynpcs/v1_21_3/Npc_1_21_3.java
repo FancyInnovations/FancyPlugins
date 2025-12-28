4165cc2302774b020a36b27b679fbf36f53e8efc
Oliver <oschi1303@gmail.com>
2025-12-28 18:44:09 +0100

    fancynpcs: Check if MiniMessage component is valid before sending packets

--- plugins/fancynpcs/implementation_1_21_3/src/main/java/de/oliver/fancynpcs/v1_21_3/Npc_1_21_3.java
+++ plugins/fancynpcs/implementation_1_21_3/src/main/java/de/oliver/fancynpcs/v1_21_3/Npc_1_21_3.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableList;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
 import com.mojang.datafixers.util.Pair;
+import com.mojang.serialization.JavaOps;
 import de.oliver.fancylib.ReflectionUtils;
 import de.oliver.fancynpcs.api.FancyNpcsPlugin;
 import de.oliver.fancynpcs.api.Npc;
@@ -16,6 +17,7 @@ import net.minecraft.Optionull;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.ComponentSerialization;
 import net.minecraft.network.chat.RemoteChatSession;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.*;
@@ -234,6 +236,14 @@ public class Npc_1_21_3 extends Npc {
 
         net.kyori.adventure.text.Component displayName = ModernChatColorHandler.translate(data.getDisplayName(), serverPlayer.getBukkitEntity());
         Component vanillaComponent = PaperAdventure.asVanilla(displayName);
+
+        // Validate MiniMessage syntax
+        try {
+            ComponentSerialization.CODEC.encodeStart(JavaOps.INSTANCE, vanillaComponent);
+        } catch (Exception e) {
+            vanillaComponent = Component.literal("Invalid displayname (check MiniMessage syntax)");
+        }
+
         if (!(npc instanceof ServerPlayer)) {
             npc.setCustomName(vanillaComponent);
             npc.setCustomNameVisible(true);

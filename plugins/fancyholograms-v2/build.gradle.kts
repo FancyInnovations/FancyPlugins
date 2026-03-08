57bf519aa2f3b400e72089515c9c7e970c0bfad6
Oliver <oschi1303@gmail.com>
2026-03-08 01:39:22 +0100

    Add support for 26.1

--- plugins/fancyholograms-v2/build.gradle.kts
+++ plugins/fancyholograms-v2/build.gradle.kts
@@ -108,7 +108,7 @@ tasks {
 
     compileJava {
         options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
-        options.release = 21
+        options.release = 25
         // For cloud-annotations, see https://cloud.incendo.org/annotations/#command-components
         options.compilerArgs.add("-parameters")
     }
@@ -141,7 +141,7 @@ tasks {
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
 }
 
 val gitCommitHash: Provider<String> = providers.exec {

544f66a6b5e6ad161f9076eddb5126625c3f9e33
Oliver <oschi1303@gmail.com>
2026-04-07 20:04:06 +0200

    packets: Rename 26.1 module

--- libraries/packets/test-plugin/build.gradle.kts
+++ libraries/packets/test-plugin/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
 
     implementation(project(":libraries:packets"))
     implementation(project(":libraries:packets:packets-api"))
-    implementation(project(":libraries:packets:implementations:26_1"))
+    implementation(project(":libraries:packets:implementations:26_1_1"))
     implementation("de.oliver.FancyAnalytics:logger:0.0.8")
 }
 

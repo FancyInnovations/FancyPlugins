c1bdd4603b2cf5f8fa8c08db5bf941b2ae92861d
Oliver <oschi1303@gmail.com>
2026-04-22 16:08:01 +0200

    fancynpcs: Add option to provide possible values via Supplier in NpcAttribute

--- plugins/fancynpcs-v2/fn-v2-api/src/main/java/de/oliver/fancynpcs/api/NpcAttribute.java
+++ plugins/fancynpcs-v2/fn-v2-api/src/main/java/de/oliver/fancynpcs/api/NpcAttribute.java
@@ -4,27 +4,46 @@ import org.bukkit.entity.EntityType;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.Supplier;
 
 public class NpcAttribute {
 
     private final String name;
     private final List<String> possibleValues;
+    private final Supplier<List<String>> possibleValuesSupplier;
     private final List<EntityType> types;
     private final BiConsumer<Npc, String> applyFunc; // npc, value
 
     public NpcAttribute(String name, List<String> possibleValues, List<EntityType> types, BiConsumer<Npc, String> applyFunc) {
         this.name = name;
         this.possibleValues = possibleValues;
+        this.possibleValuesSupplier = List::of;
+        this.types = types;
+        this.applyFunc = applyFunc;
+    }
+
+    public NpcAttribute(String name, Supplier<List<String>> possibleValuesSupplier, List<EntityType> types, BiConsumer<Npc, String> applyFunc) {
+        this.name = name;
+        this.possibleValues = List.of();
+        this.possibleValuesSupplier = possibleValuesSupplier;
         this.types = types;
         this.applyFunc = applyFunc;
     }
 
     public boolean isValidValue(String value) {
-        if (possibleValues.isEmpty()) {
+        List<String> fromSupplier = possibleValuesSupplier.get();
+
+        if (possibleValues.isEmpty() && fromSupplier.isEmpty()) {
             return true;
         }
 
         for (String pv : possibleValues) {
+            if (pv.equalsIgnoreCase(value)) {
+                return true;
+            }
+        }
+
+        for (String pv : fromSupplier) {
             if (pv.equalsIgnoreCase(value)) {
                 return true;
             }

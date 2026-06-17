871978013382a02f1c8a12b13eb13d64d793f373
Oliver <oschi1303@gmail.com>
2026-06-17 15:23:43 +0200

    Update lamp to 4.0.0-rc.17

--- plugins/fancyworlds/build.gradle.kts
+++ plugins/fancyworlds/build.gradle.kts
@@ -43,8 +43,8 @@ dependencies {
 
     compileOnly("de.oliver:FancyDialogs:1.1.2")
     compileOnly("org.lushplugins.chatcolorhandler:paper:8.1.1")
-    implementation("io.github.revxrsal:lamp.common:4.0.0-rc.16")
-    implementation("io.github.revxrsal:lamp.bukkit:4.0.0-rc.16")
+    implementation("io.github.revxrsal:lamp.common:4.0.0-rc.17")
+    implementation("io.github.revxrsal:lamp.bukkit:4.0.0-rc.17")
 
     implementation("org.jetbrains:annotations:26.1.0")
 }

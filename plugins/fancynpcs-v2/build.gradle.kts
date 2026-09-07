291e1f827693e58606870fca50537105d34a2ff0
Oliver <oschi1303@gmail.com>
2026-09-07 12:36:06 +0200

    Update to 26.3-pre-2

--- plugins/fancynpcs-v2/build.gradle.kts
+++ plugins/fancynpcs-v2/build.gradle.kts
@@ -94,8 +94,8 @@ paper {
 
 tasks {
     runServer {
-        minecraftVersion("26.3-pre-1")
-        serverJar(file("/Users/oliver/Workspace/paper/paper-server/build/libs/paper-bundler-26.3-pre-1.build.1-alpha.jar"))
+        minecraftVersion("26.3-pre-2")
+        serverJar(file("/Users/oliver/Workspace/paper/paper-server/build/libs/paper-bundler-26.3-pre-2.build.1-alpha.jar"))
 
 
         downloadPlugins {
